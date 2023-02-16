36d01dff030794734b1017496165be5996309693
rdiehlmartinez <richarddiehl@gmail.com>
2023-02-16 16:11:44 +0000

    adding offline_mode to run_model entry point and enable use of environ variables

--- run_model.py
+++ run_model.py
@@ -10,19 +10,23 @@ from src.utils import setup
 from src.pipeline import Pipeline
 
 parser = argparse.ArgumentParser(description="Parses config files passed in via CLI")
-parser.add_argument("Path", metavar="path", type=str, help="path to the config file")
-parser.add_argument(
-    "--run_id", type=str, help="""Unique identifier for the run of the model"""
-)
+parser.add_argument("Path", metavar="path", type=str, help="path to the config yaml file")
+parser.add_argument("--run_id", type=str, help="Unique identifier for the run of the model")
+parser.add_argument("--offline_mode", action="store_true", help="Run the model without pushing to wandb")
 args = parser.parse_args()
 
 if __name__ == "__main__":
 
     # Setting up an id for the specific run
-    if args.run_id is None:
+    if args.run_id is None and "RUN_ID" not in os.environ:
+        # the user has not specified a run id, so we will generate one
         run_id = str(random.randint(1, 1e9))
     else:
-        run_id = args.run_id
+        if args.run_id is not None:
+            run_id = args.run_id
+        else:
+            # run_id can also be exported as an environment variable
+            run_id = os.environ["RUN_ID"]
 
     # Possibly reading in a runfile (only exists if we are resuming training)
     run_file_path = f"tmp/{run_id}.runfile"
@@ -34,10 +38,10 @@ if __name__ == "__main__":
         resume_num_task_batches = 0
 
     # Setting up logging, config read in and seed setting
-    config = setup(args.Path, run_id, resume_num_task_batches)
+    setup(args.Path, run_id, resume_num_task_batches, args.offline_mode)
 
     # Initializing the modeling pipeline with configuration and options
-    pipeline = Pipeline(config, resume_num_task_batches)
+    pipeline = Pipeline(resume_num_task_batches)
 
     # setting up timeout handler - called if the program receives a SIGINT either from the user
     # or from SLURM if it is about to timeout
